694ca23490821061ae1b6ee5fbb328bc10695e13
ezambomsantana <ezambomsantana@gmail.com>
2016-09-11 14:35:50 -0300

    Create start_simulator_collectl.sh

---
+++ scripts/start_simulator_collectl.sh
@@ -0,0 +1,13 @@
+echo "Start collectl"
+
+collectl -scj  -P -f/home/eduardo/ &
+
+echo "Start SCSimulator"
+
+cd /home/eduardo/Software/sim2/mock-simulators/smart-city/src/
+
+make smart_city_run CMD_LINE_OPT="--batch"
+
+echo "Finish collectl"
+
+ps -ef | grep collectl | grep -v grep | awk '{print $2}' | xargs kill
